8ebd09773b7fb3a62d1d436d39a3d832413f0da6
tinkerhao <tinkerhao@tencent.com>
2020-07-21 16:40:30 +0800

    impr: 修正缩进

--- css/滑动标签/index.js
+++ css/滑动标签/index.js
@@ -5,22 +5,22 @@
 
 var lastSelected = 0
 
-window.onload = function() {
-    var unitItems = document.getElementsByClassName('unit-item')
-    for (let i = 0; i < unitItems.length; i++) {
-        unitItems[i].addEventListener('click', () => {
-            itemClick(i, unitItems)
-        })
-    }
+window.onload = function () {
+  var unitItems = document.getElementsByClassName('unit-item')
+  for (let i = 0; i < unitItems.length; i++) {
+    unitItems[i].addEventListener('click', () => {
+      itemClick(i, unitItems)
+    })
+  }
 
-    var slider = document.getElementById('unit-slider')
-    slider.style = `width: ${unitItems[0].offsetWidth}px; transform: translateX(${unitItems[0].offsetLeft}px)`
+  var slider = document.getElementById('unit-slider')
+  slider.style = `width: ${unitItems[0].offsetWidth}px; transform: translateX(${unitItems[0].offsetLeft}px)`
 }
 
 function itemClick(idx, unitItems) {
-    unitItems[lastSelected].classList.remove('selected')
-    unitItems[idx].classList.add('selected')
-    lastSelected = idx
-    var slider = document.getElementById('unit-slider')
-    slider.style = `width: ${unitItems[idx].offsetWidth}px; transform: translateX(${unitItems[idx].offsetLeft}px)`
+  unitItems[lastSelected].classList.remove('selected')
+  unitItems[idx].classList.add('selected')
+  lastSelected = idx
+  var slider = document.getElementById('unit-slider')
+  slider.style = `width: ${unitItems[idx].offsetWidth}px; transform: translateX(${unitItems[idx].offsetLeft}px)`
 }
--- css/瀑布流/index.js
+++ css/瀑布流/index.js
@@ -4,55 +4,55 @@
  * 2. 内容从左向右排列, 下一行的内容有限排在较短的列下
  */
 
- // 4 列
- var colNum = 4
+// 4 列
+var colNum = 4
 
- window.onload = function() {
-    waterfall()
- }
-
- window.onresize = function () {
-    waterfall();
+window.onload = function () {
+  waterfall()
 }
 
- function getClientWH() {
-    return {
-        width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-        height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-    }
+window.onresize = function () {
+  waterfall();
 }
 
- function waterfall() {
-    var waterfallItems = document.getElementsByClassName('waterfall-item')
+function getClientWH() {
+  return {
+    width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+  }
+}
 
-    // 定义宽度和间距
-    var pageW = getClientWH().width
-    var itemW = waterfallItems[0].offsetWidth
-    var marginLeft = parseInt(pageW / 100)
-    var marginBottom = 8
-    // 保存每列当前高度
-    var hArr = []
-    for (var i = 0; i < waterfallItems.length; i++) {
-        // 第一行
-        if (i < colNum) {
-            waterfallItems[i].style.top = 0
-            waterfallItems[i].style.left = (itemW + marginLeft) * i + 'px'
-            hArr.push(waterfallItems[i].offsetHeight)
-        } else {
-            // 寻找高度最低的列
-            var minHeightColIdx = 0
-            for(var j = 0; j < hArr.length; j++) {
-                if (hArr[minHeightColIdx] > hArr[j]) {
-                    minHeightColIdx = j
-                }
-            }
+function waterfall() {
+  var waterfallItems = document.getElementsByClassName('waterfall-item')
 
-            waterfallItems[i].style.top = (hArr[minHeightColIdx] + marginBottom) + 'px'
-            waterfallItems[i].style.left = waterfallItems[minHeightColIdx].style.left
-
-            // 累计列高度
-            hArr[minHeightColIdx] += waterfallItems[i].offsetHeight + marginBottom
-            console.log(hArr)
+  // 定义宽度和间距
+  var pageW = getClientWH().width
+  var itemW = waterfallItems[0].offsetWidth
+  var marginLeft = parseInt(pageW / 100)
+  var marginBottom = 8
+  // 保存每列当前高度
+  var hArr = []
+  for (var i = 0; i < waterfallItems.length; i++) {
+    // 第一行
+    if (i < colNum) {
+      waterfallItems[i].style.top = 0
+      waterfallItems[i].style.left = (itemW + marginLeft) * i + 'px'
+      hArr.push(waterfallItems[i].offsetHeight)
+    } else {
+      // 寻找高度最低的列
+      var minHeightColIdx = 0
+      for (var j = 0; j < hArr.length; j++) {
+        if (hArr[minHeightColIdx] > hArr[j]) {
+          minHeightColIdx = j
         }
+      }
+
+      waterfallItems[i].style.top = (hArr[minHeightColIdx] + marginBottom) + 'px'
+      waterfallItems[i].style.left = waterfallItems[minHeightColIdx].style.left
+
+      // 累计列高度
+      hArr[minHeightColIdx] += waterfallItems[i].offsetHeight + marginBottom
+      console.log(hArr)
     }
- }
+  }
+}
