e3cf5219b027843bd9b045691c8d1fe3da862915
GStarP <171250605@smail.nju.edu.cn>
2022-03-10 14:12:42 +0800

    feat: typeof&instanceof

--- js/base.js
+++ js/base.js
@@ -1,4 +1,3 @@
-
 /**
  * Here are things you should know
  */
@@ -7,78 +6,84 @@
  * String.trim()
  * 去除首位空格
  */
-console.log(' abc hxw '.trim())  // abc_hxw
+console.log(' abc hxw '.trim()); // abc_hxw
 
 /**
  * typeof
  */
-console.log(typeof 1)          // number
-console.log(typeof '1')        // string
-console.log(typeof true)       // boolean
-console.log(typeof {})         // object
-console.log(typeof [])         // object
-console.log(typeof undefined)  // undefined
-console.log(typeof null)       // object
+console.log(typeof 1); // number
+console.log(typeof '1'); // string
+console.log(typeof true); // boolean
+function func() {}
+console.log(typeof func); // function
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
 
 /**
  * instanceof
+ * a instanceof B 测试 B.prototype 是否在 Object.getPrototype(a) 原型链上
+ * Object.getPrototype(Object.getPrototype(a)) 终会回溯到 Object.prototype
  */
-const arr = []
-console.log(arr instanceof Array)   // true
-console.log(arr instanceof Object)  // true
+const arr = [];
+console.log(arr instanceof Array); // true
+console.log(arr instanceof Object); // true
 function HXW() {}
-const p = new HXW()
-console.log(p instanceof HXW)       // true
+const p = new HXW();
+console.log(p instanceof HXW); // true
+console.log(p instanceof Object); // true
 
 /**
  * Object.prototype.toString
+ * 用于获取 [[Class]]
  */
-const arr = []
-console.log(Object.prototype.toString.call(arr))  // [object Array]
+const arr = [];
+console.log(Object.prototype.toString.call(arr)); // [object Array]
 function HXW() {}
-const p = new HXW()
-console.log(Object.prototype.toString.call(p))   // [object Object]
+const p = new HXW();
+console.log(Object.prototype.toString.call(p)); // [object Object]
 
 /**
  * sort
  */
-const arr = [5, 3, 6, 4, 1]
-console.log(arr.sort())
-console.log(arr.sort((a, b) => b-a))
+const arr = [5, 3, 6, 4, 1];
+console.log(arr.sort());
+console.log(arr.sort((a, b) => b - a));
 
 /**
  * for loop
  */
 // for in
-const arr = [5, 4, 3]
+const arr = [5, 4, 3];
 for (let i in arr) {
-  console.log(arr[i])
+  console.log(arr[i]);
 }
 function Sup() {
-  this.name = 'hxw'
+  this.name = 'hxw';
 }
 function Sub() {
-  this.gf = 'zzn'
-  this.age = 18
+  this.gf = 'zzn';
+  this.age = 18;
 }
-Sub.prototype = new Sup()
-const sub = new Sub()
+Sub.prototype = new Sup();
+const sub = new Sub();
 for (let key in sub) {
-  console.log(`${key}: ${sub[key]}`)
+  console.log(`${key}: ${sub[key]}`);
 }
 // forEach
-const arr = [1, 2, 3]
+const arr = [1, 2, 3];
 arr.forEach((e, i) => {
   if (i === 2) {
     // break is illegal
     // break
   }
-  console.log(`${i}: ${e}`)
-})
+  console.log(`${i}: ${e}`);
+});
 // for of
-let m = new Map()
-m.set('gf1', 'zzn')
-m.set('gf2', 'lxc')
+let m = new Map();
+m.set('gf1', 'zzn');
+m.set('gf2', 'lxc');
 for (let [k, v] of m) {
-  console.log(`${k}: ${v}`)
+  console.log(`${k}: ${v}`);
 }
